129c3cfa6b2aab2cf8b81eb0adf1a17386a7d797
Danil <hassie54men@icloud.com>
2025-08-28 23:42:04 +0300

    add ReactRouter.tsx element to main App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { CharacterList } from "./features/characters/components/CharacterList.tsx";
-import LocationList from "./features/locationst/components/LocationList.tsx";
+// import { CharacterList } from "./features/characters/components/CharacterList.tsx";
+// import LocationList from "./features/locationst/components/LocationList.tsx";
+import ReactRouter from "./features/router/components/ReactRouter.tsx";
 
 function App() {
   return (
     <>
-      <CharacterList />
-      <LocationList />
+      {/*<CharacterList />*/}
+      {/*<LocationList />*/}
+      <ReactRouter />
     </>
   );
 }
