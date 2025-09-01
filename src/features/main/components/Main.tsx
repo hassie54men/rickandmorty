e856bf474628557637dbde0d61e8909f48a17445
Danil <hassie54men@icloud.com>
2025-09-01 12:41:22 +0300

    add Main.tsx

---
+++ src/features/main/components/Main.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+export function Main() {
+  return (
+    <Box
+      component={"main"}
+      sx={{ display: "flex", justifyContent: "center", marginBlock: "20px" }}
+    >
+      <img
+        src="https://rick-i-morty.online/wp-content/uploads/2021/06/5-sezon.jpg"
+        alt=""
+        width="494"
+        height="741"
+      />
+    </Box>
+  );
+}
