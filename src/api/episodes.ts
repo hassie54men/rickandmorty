c57567be1e4d508a9fe950f714a9e99453ed8a73
Danil <hassie54men@icloud.com>
2025-09-02 23:04:24 +0300

    remove console.log, remove if() check, replaced axios response in getEpi func

--- src/api/episodes.ts
+++ src/api/episodes.ts
@@ -1,35 +1,26 @@
 import { apiClient } from "./apiClient";
 import { ENDPOINTS } from "./constants";
-import type { AxiosResponse } from "axios";
-import type { EpisodeListResponse } from "./types/episodes";
+import { type AxiosResponse } from "axios";
+import type { Episode, EpisodeListResponse } from "./types/episodes";
 
 export async function getEpisodes() {
   try {
     const res: AxiosResponse<EpisodeListResponse> = await apiClient.get(
       ENDPOINTS.episodes,
     );
-    console.log(res);
-    if (res) {
-      return res.data;
-    } else {
-      throw new Error("ошибка");
-    }
-  } catch (e) {
-    console.error(e);
+    return res.data;
+  } catch (error) {
+    console.error(error);
   }
 }
 
 export async function getEpisode(id: number) {
   try {
-    const res: AxiosResponse<EpisodeListResponse> = await apiClient.get(
+    const res: AxiosResponse<Episode> = await apiClient.get(
       ENDPOINTS.episode(id),
     );
-    if (res) {
-      return res.data;
-    } else {
-      throw new Error("Error!");
-    }
-  } catch (e) {
-    console.error(e);
+    return res.data;
+  } catch (error) {
+    console.error("Unexpected error:", error);
   }
 }
