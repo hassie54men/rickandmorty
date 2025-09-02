969a9beb353707c53c6adeb8ddb1040e70f15d0d
Danil <hassie54men@icloud.com>
2025-09-02 23:01:16 +0300

    remove console.log

--- src/features/episodes/components/EpisodeDetail.tsx
+++ src/features/episodes/components/EpisodeDetail.tsx
@@ -11,7 +11,6 @@ export function EpisodeDetail() {
 
   useEffect(() => {
     getEpisode(episodeId).then((episode) => {
-      console.log(episode);
       if (episode) {
         setEpisode(episode);
       }
