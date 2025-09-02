dc516faf80d016f894093e70a81646031805d262
Danil <hassie54men@icloud.com>
2025-09-02 22:58:09 +0300

    remove input and button from Main.tsx

--- src/features/main/components/Main.tsx
+++ src/features/main/components/Main.tsx
@@ -1,19 +1,6 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { getCharacterSearch } from "../../../api/characters";
+import { Box } from "@mui/material";
 
 export function Main() {
-  const [value, setValue] = useState<string>();
-  const [searchName, setSearchName] = useState<string>();
-
-  useEffect(() => {
-    getCharacterSearch(value).then((item) => {
-      console.log(item);
-      if (item) {
-        setSearchName(item);
-      }
-    });
-  }, []);
   return (
     <Box
       component={"main"}
@@ -26,28 +13,12 @@ export function Main() {
         gap: "20px",
       }}
     >
-      <Box
-        component={"div"}
-        sx={{ display: "flex", justifyContent: "space-between" }}
-      >
-        <TextField
-          fullWidth
-          label="Search personage"
-          id="personage"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <Button variant="outlined" type={"submit"}>
-          Search
-        </Button>
-      </Box>
       <img
         src="https://rick-i-morty.online/wp-content/uploads/2021/06/5-sezon.jpg"
         alt=""
         width="494"
         height="741"
       />
-      <Typography>{value}</Typography>
     </Box>
   );
 }
