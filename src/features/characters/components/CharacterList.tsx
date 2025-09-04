668fe0f37d9dfd237f3154f5dbb5aac089175f87
Danil <hassie54men@icloud.com>
2025-09-04 23:53:46 +0300

    add page buttons

--- src/features/characters/components/CharacterList.tsx
+++ src/features/characters/components/CharacterList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { Character } from "../../../api/types/characters";
 import { getCharacters } from "../../../api/characters";
 import { CharacterCard } from "./CharacterCard";
-import { Box, Button, Grid, TextField } from "@mui/material";
+import { Box, Button, ButtonGroup, Grid, TextField } from "@mui/material";
 
 export function CharacterList() {
   const [characters, setCharacters] = useState<Character[]>([]);
@@ -10,10 +10,24 @@ export function CharacterList() {
   const [error, setError] = useState<string | null>(null);
   const [value, setValue] = useState<string>("");
   const [search, setSearch] = useState<string>();
+  const [page, setPage] = useState(1);
+
+  function nextPage() {
+    if (page < 42) {
+      setPage(page + 1);
+      console.log(page);
+    }
+  }
+  function previousPage() {
+    setPage(page - 1);
+    if (page === 1) {
+      setPage(page);
+    }
+  }
 
   useEffect(() => {
     setIsLoading(true);
-    getCharacters({ name: search })
+    getCharacters({ name: search, page: page })
       .then((res) => {
         if (res) {
           console.log(res.results);
@@ -27,7 +41,7 @@ export function CharacterList() {
         setError(e?.message || "error");
       })
       .finally(() => setIsLoading(false));
-  }, [search]);
+  }, [search, page]);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -60,6 +74,15 @@ export function CharacterList() {
         >
           Search
         </Button>
+      </Box>
+      <Box
+        component={"div"}
+        sx={{ display: "flex", justifyContent: "center", marginBlock: "20px" }}
+      >
+        <ButtonGroup variant="outlined" aria-label="Basic button group">
+          <Button onClick={nextPage}>Next</Button>
+          <Button onClick={previousPage}>Previous</Button>
+        </ButtonGroup>
       </Box>
       <Grid container spacing={3} sx={{ marginBlock: "20px" }}>
         {characters.map((item) => (
