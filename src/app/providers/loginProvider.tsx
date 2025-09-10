1cc6bba92dd7c893c55bff29e87b06cdc27479d5
Danil <hassie54men@icloud.com>
2025-09-10 22:55:51 +0300

    created loginProvider looking at the written code and tried to write it myself

---
+++ src/app/providers/loginProvider.tsx
@@ -0,0 +1,72 @@
+import { createContext, type ReactNode, useEffect, useState } from "react";
+interface LoginProps {
+  children: ReactNode;
+}
+
+interface LoginContextType {
+  user: string;
+  isLog: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export const LoginContext = createContext<LoginContextType | null>(null);
+
+export const LoginProvider = (props: LoginProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLog, setIsLog] = useState(false);
+  const USER_KEY = "key";
+  const { children } = props;
+
+  useEffect(() => {
+    const loginStorage = localStorage.getItem(USER_KEY);
+    if (loginStorage) {
+      const localStorageObj: User = JSON.parse(loginStorage);
+      setUser(localStorageObj);
+      setIsLog(true);
+    } else {
+      setUser(null);
+      setIsLog(false);
+    }
+  }, []);
+
+  const login = (email: string, password: string) => {
+    const adminEmail: string = "admin@mail.ru";
+    const adminPassword: string = "admin";
+    const adminUser = {
+      id: "1",
+      name: "Danil",
+      email: "admin@mail.ru",
+    };
+
+    if (adminEmail === email && adminPassword === password) {
+      setUser(adminUser);
+      setIsLog(true);
+      localStorage.setItem(USER_KEY, JSON.stringify(adminUser));
+    } else {
+      setUser(null);
+      setIsLog(false);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setIsLog(false);
+    localStorage.removeItem(USER_KEY);
+  };
+
+  const value = {
+    user: user,
+    isLog: isLog,
+    login,
+    logout,
+  };
+
+  return <LoginContext value={value}>{children}</LoginContext>;
+};
