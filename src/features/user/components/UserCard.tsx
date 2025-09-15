977238ac79baac7891469d1502506c365d8def75
hassie54men <hassie54men@gmail.com>
2025-09-15 23:41:58 +0300

    add answer if islog = false and add check useLogin != false

---
+++ src/features/user/components/UserCard.tsx
@@ -0,0 +1,46 @@
+
+import {Avatar, Box, Button, Typography} from "@mui/material";
+import {useNavigate} from "react-router";
+import {appRoutes} from "../../../app/router/routes";
+import {useLogin} from "../../../app/hooks/hooks";
+
+export function UserCard() {
+  const context = useLogin()
+  if(!context){
+    throw new Error('error')
+  }
+  const { user, logout, isLog } = context;
+  const navigate = useNavigate()
+  function handleNavigate(){
+    navigate(appRoutes.login)
+  }
+  function handleNavigateInError(){
+    navigate(appRoutes.login)
+  }
+
+  function handleLogout(){
+    logout();
+    handleNavigate()
+  }
+
+  if(!isLog){
+    return <Box sx={{display: 'flex', justifyContent: 'center', flexDirection:'column', alignItems: 'center', gap: '10px', marginBlock: '10px'}}>
+      <Typography>неверный логин или пароль</Typography>
+      <Button variant="outlined" onClick={handleNavigateInError}>
+        Logout
+      </Button>
+    </Box>
+  }
+  return(
+     <Box sx={{
+       marginBlock: "20px",
+     }}>
+       <Avatar src='https://1avatara.ru/pic/animation/animation0036.gif' sx={{width: '100px', height: '100px'}}></Avatar>
+       <p>Hello {user.name}</p>
+       <p>Your email: {user.email}</p>
+       <Button variant="outlined" onClick={handleLogout}>
+         Logout
+       </Button>
+     </Box>
+  )
+}
